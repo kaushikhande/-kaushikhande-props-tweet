1803eb8dff2d4ec041555bb2b6f9ddb24fe54c44
kaushikhande <kaushik.hande.vc@startv.com>
2021-03-28 16:46:28 +0530

    tweet ui with different components.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function Avatar() {
+  return (
+    <img
+      src="https://www.gravatar.com/avatar/nothing"
+      className="avatar"
+      alt="avatar" />
+  );
+}
+
+function Message() {
+  return (
+    <div className="message">
+      This is less than 140 characters.
+    </div>
+  );
+}
+
+function NameWithHandle() {
+  return (
+    <span className="name-with-handle">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  );
+}
+
+const Time = () => (
+  <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button"/>
+);
+
+const RetweetButton = () => (
+  <i className="fa fa-retweet retweet-button"/>
+);
+
+const LikeButton = () => (
+  <i className="fa fa-heart like-button"/>
+);
+
+function Tweet() {
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <NameWithHandle /><Time/>
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button"/>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Tweet />
   </React.StrictMode>,
   document.getElementById('root')
 );
